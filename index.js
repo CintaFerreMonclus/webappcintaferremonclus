0618fcc77e7bc281c9844499b8fdb17edeaa1745
CintaFerreMonclus <cinta.ferre@institutroquetes.cat>
2025-04-25 11:59:36 +0200

    d

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function canvia_seccio(num_boto) {
         let boto = document.getElementById("boto_" + i);
         let seccio = document.getElementById("seccio_" + i);
         if (i == num_boto) {
-            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
-            boto.style.backgroundColor = "#FCDEE0";
+            boto.style.color = "#8707FF";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+            boto.style.backgroundColor = "#82C9F8";
             seccio.style.display = "flex";    // es fa visible la secció activa
         }
         else {
